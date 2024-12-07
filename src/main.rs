9673cec8ad175d6d97bb61ce2fb5bdb89cbd5750
BekirKagan <kagankaraahmetoglu@hotmail.com>
2024-12-07 19:44:36 +0300

    Improved the init command.

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,82 @@
 mod args;
 use args::ArcanumCommands;
 
-use dirs_next::{data_dir, data_local_dir};
-use std::{
-    fs::File,
-    io::Write,
-    path::{Path, PathBuf},
-};
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+use std::path::PathBuf;
+
+use dirs_next::data_local_dir;
 
 use rpassword::prompt_password;
 
-use argon2::{
-    password_hash::{rand_core::OsRng, SaltString},
-    Argon2, PasswordHash, PasswordHasher,
-};
+use argon2::password_hash::rand_core::OsRng;
+use argon2::password_hash::PasswordHasher;
+use argon2::password_hash::SaltString;
+use argon2::Argon2;
 
-const DATA_FILENAME: &str = "arcanum.data";
+use serde_json::json;
+
+const DATA_FILENAME: &str = "arcanum.json";
 
 fn get_data_filepath() -> PathBuf {
-    let data_dir = data_local_dir().unwrap_or(data_dir().expect("Failed to get data directory."));
-    let data_filepath = Path::join(&data_dir, DATA_FILENAME);
-    data_filepath
+    let directory = data_local_dir().expect("Could not find data directory.");
+    let filepath = Path::join(directory.as_path(), DATA_FILENAME);
+    filepath
+}
+
+fn should_initialize() -> bool {
+    let filepath = get_data_filepath();
+    let should_initialize = filepath.try_exists().unwrap_or_default();
+    !should_initialize
+}
+
+fn atomic_write_all(path: PathBuf, data: &[u8]) {
+    let mut temp = File::create(path.with_extension("tmp"))
+        .expect("Failed to create the temporary data file.");
+
+    temp.write_all(data)
+        .expect("Failed to write to the temporary data file.");
+
+    temp.sync_all()
+        .expect("Failed to sync the temporary data file.");
+
+    std::fs::rename(path.with_extension("tmp"), path)
+        .expect("Failed to rename the temporary data file.");
+}
+
+fn initialize_vault() {
+    let password =
+        prompt_password("Enter master password:").expect("Failed to retrieve master password.");
+
+    let arg2 = Argon2::default();
+
+    let salt = SaltString::generate(&mut OsRng);
+    let hash = arg2
+        .hash_password(password.as_bytes(), &salt)
+        .expect("Failed to hash master password.");
+
+    let data = json!({
+        "version": "0.1.0",
+        "salt": salt.as_str(),
+        "hash": hash.to_string(),
+    });
+
+    atomic_write_all(get_data_filepath(), data.to_string().as_bytes());
+
+    println!("Initialization successful.");
 }
 
 fn main() {
     let command = args::parse_command();
 
+    #[allow(unused_variables)]
     match command {
         ArcanumCommands::Init => {
-            let filepath = get_data_filepath();
-
-            match File::create_new(&filepath) {
-                Ok(mut file) => {
-                    let password = prompt_password("Enter master password:")
-                        .expect("Failed to get master password.");
-
-                    let salt = SaltString::generate(&mut OsRng);
-                    let arg2 = Argon2::default();
-
-                    let hash_string = arg2
-                        .hash_password(password.as_bytes(), &salt)
-                        .expect("Failed to hash master password.")
-                        .to_string();
-
-                    let hash = PasswordHash::new(&hash_string)
-                        .expect("Failed to generate hash.")
-                        .to_string();
-
-                    file.write_all(hash.as_bytes())
-                        .expect("Failed to save hash.");
-
-                    println!("Successfully initialized.");
-                }
-
-                Err(error) => {
-                    if error.kind() == std::io::ErrorKind::AlreadyExists {
-                        println!("Already initialized!");
-                    } else {
-                        println!(
-                            "Failed to create data file in directory: {}",
-                            filepath.to_str().unwrap()
-                        );
-                    }
-                }
+            if should_initialize() {
+                initialize_vault();
+            } else {
+                println!("Already initialized.");
             }
         }
 
