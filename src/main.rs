9859edc5cbaa3ff52ca0db39d5b6f1fe523e8607
BekirKagan <kagankaraahmetoglu@hotmail.com>
2025-01-23 19:56:27 +0300

    Restructured 'main' function as 'data.rs' module does not exist anymore.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,32 @@
 mod app;
 mod args;
 mod auth;
-mod data;
+mod storage;
+mod vault;
 
 use anyhow::anyhow;
 use anyhow::Result;
 use args::Command;
+use storage::Storage;
+use vault::Credential;
+use vault::Vault;
 
 fn main() -> Result<()> {
-    let initialized = data::vault_exists()?;
     let command = args::get_command();
+
+    let storage = Storage::new()?;
+    let initialized = storage.vault_exists()?;
 
     if command != Command::Init {
         if !initialized {
             return Err(anyhow!("Arcanum is not initialized."));
         }
 
-        if !auth::is_authorized()? {
+        let vault = storage.load_vault()?;
+        let master_key = vault.get_master_key()?;
+        let authorized = auth::is_authorized(master_key)?;
+
+        if !authorized {
             return Err(anyhow!("Permission denied."));
         }
     }
@@ -29,43 +39,37 @@ fn main() -> Result<()> {
 
             let master_password = auth::get_master_password()?;
             let master_key = auth::generate_master_key(master_password, None)?;
-
-            data::vault_initialize(master_key)?;
-
+            let vault = Vault::new(master_key.hash, master_key.salt);
+            storage.initialize()?;
+            storage.save_vault(vault)?;
             println!("Arcanum is initialized successfully.");
         }
 
         Command::Add => {
-            let credential = data::get_credential()?;
-            data::vault_add_credential(credential)?;
-
+            let credential = Credential::prompt()?;
+            let mut vault = storage.load_vault()?;
+            vault.add_credential(credential)?;
+            storage.save_vault(vault)?;
             println!("Added credential successfully.");
         }
 
         Command::Remove => {
-            let credential_name = data::get_credential_name()?;
-            data::vault_remove_credential(credential_name)?;
-
+            let name = Credential::prompt_name()?;
+            let mut vault = storage.load_vault()?;
+            vault.remove_credential(&name)?;
+            storage.save_vault(vault)?;
             println!("Removed credential successfully.");
         }
 
         Command::List => {
-            let credentials = data::vault_get_credentials()?;
-
-            if credentials.is_empty() {
-                println!("There are no credentials in the vault.");
-            } else {
-                for credential in credentials {
-                    println!("{}", "-".repeat(50));
-                    println!("({}) {}", credential.name, credential.url,);
-                    println!("{}", "-".repeat(50));
-                }
+            let vault = storage.load_vault()?;
+            for credential in vault.get_credentials()? {
+                println!("{credential}");
             }
         }
 
         Command::Reset => {
-            data::vault_reset()?;
-
+            storage.remove_vault()?;
             println!("Arcanum is resetted successfully.");
         }
     }
